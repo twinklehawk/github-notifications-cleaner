62e8236236a180375f69ebcb421c8bf52a7b3ec0
Alex Daidone <alex@plshark.net>
2025-09-23 13:40:36 -0400

    Support all request options for fetching notifications

--- github-client/src/main/kotlin/net/plshark/github/client/notifications/NotificationsClient.kt
+++ github-client/src/main/kotlin/net/plshark/github/client/notifications/NotificationsClient.kt
@@ -4,11 +4,12 @@ import kotlinx.coroutines.flow.Flow
 
 /** A client interface for interacting with GitHub notifications. */
 interface NotificationsClient {
-    /** Lists all notifications for the current user, sorted by most recently updated. */
-    fun getNotifications(): Flow<Notification>
-
-    /** Lists all unread notifications for the current user, sorted by most recently updated. */
-    fun getUnreadNotifications(): Flow<Notification>
+    /**
+     * Lists notifications for the current user, sorted by most recently updated.
+     *
+     * This method ignores the page value in the request and returns all results.
+     */
+    fun getNotifications(request: GetNotificationsRequest): Flow<Notification>
 
     /** Marks a thread as done. */
     suspend fun markThreadDone(threadId: Long)
